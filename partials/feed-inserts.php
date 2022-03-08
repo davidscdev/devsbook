778a0eedff013742680abfd2798236c3f0648768
davidscdev <davidsc.dev@gmail.com>
2022-03-08 08:16:16 -0300

    Estruturação do layout do partial feed-inserts e do aside direito (patrocinadores e criado por.

---
+++ partials/feed-inserts.php
@@ -0,0 +1,18 @@
+<?php
+    $firtsName = current(explode(' ', $userInfo->name));
+?>
+
+<div class="box feed-new">
+    <div class="box-body">
+            <div class="feed-new-editor m-10 row">
+                    <div class="feed-new-avatar">
+                        <img src="<?=$base;?>/media/avatars/<?=$userInfo->avatar?>" />
+                    </div>
+                    <div class="feed-new-input-placeholder">O que você está pensando, <?=$firtsName;?>?</div>
+                    <div class="feed-new-input" contenteditable="true"></div>
+                    <div class="feed-new-send">
+                     <img src="<?=$base;?>/assets/images/send.png" />
+                </div>
+            </div>
+    </div>
+</div>
