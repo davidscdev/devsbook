b6d9999bade8b278eef307f6f9a3084aa0a97d29
davidscdev <davidsc.dev@gmail.com>
2022-03-11 19:58:55 -0300

    Inicio da inserção dos dados do usuário na page de profile.

--- profile.php
+++ profile.php
@@ -24,6 +24,12 @@ if (!$id) {
 $postData = new PostDaoMysql($pdo);
 $userData = new UserDaoMysql($pdo);
 
+
+$user = $userData->findById($id);
+if(!$user){
+    header('Location: '. $base);
+}
+
 // 1 - Pegar as infos do usuário 
 
 // 2 - Pegar o feed desse usuário 
@@ -40,14 +46,14 @@ require 'partials/menu.php';
     <div class="row">
         <div class="box flex-1 border-top-flat">
             <div class="box-body">
-                <div class="profile-cover" style="background-image: url('media/covers/cover.jpg');"></div>
+                <div class="profile-cover" style="background-image: url('<?=$base;?>/media/covers/<?=$user->cover;?>');"></div>
                 <div class="profile-info m-20 row">
                     <div class="profile-info-avatar">
-                        <img src="media/avatars/avatar.jpg" />
+                        <img src="<?=$base;?>/media/avatars/<?=$user->avatar;?>" />
                     </div>
                     <div class="profile-info-name">
-                        <div class="profile-info-name-text">Bonieky Lacerda</div>
-                        <div class="profile-info-location">Campina Grande</div>
+                        <div class="profile-info-name-text"><?=$user->name;?></div>
+                        <div class="profile-info-location"><?=$user->city;?></div>
                     </div>
                     <div class="profile-info-data row">
                         <div class="profile-info-item m-width-20">
