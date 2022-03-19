eb848215401a5f9020d7a90a89e4c1ef0eb238fd
davidscdev <davidsc.dev@gmail.com>
2022-03-19 08:57:56 -0300

    Tratamento e carregamento da imagem do avatar.

--- settings-action.php
+++ settings-action.php
@@ -18,6 +18,9 @@ $city = filter_input(INPUT_POST,'city');
 $password = filter_input(INPUT_POST,'password');
 $password_confirm = filter_input(INPUT_POST,'password_confirm');
 
+
+
+
 //Verifica se o nome e a senha foram preenchidos
 if ($name && $email) {
     
@@ -70,6 +73,61 @@ if ($name && $email) {
         }    
 }
 $userInfo->email = $email;
+
+echo '<pre>';
+var_dump($_FILES);
+
+/*************************************************
+*   Trata o envio do Avatar
+*************************************************/
+//Testa se o avatar foi enviado e se não contém erro no envio
+if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] == 0 && !empty($_FILES['avatar']['tmp_name'])) {
+    $newAvatar = $_FILES['avatar'];
+    //Testa se o tipo de imagem enviado é um jpeg, jpg ou png
+    if (in_array($newAvatar['type'], ['image/jpeg','image/jpg','image/png'])) {
+        $widthAvatar = 200;
+        $heightAvatar = 200;
+
+        list($widthOrig, $heightOrig) = getimagesize($newAvatar['tmp_name']);
+        $ratio = $widthOrig / $heightOrig;
+
+        $newWidth = $widthAvatar;
+        $newHeight = $newWidth / $ratio;
+
+        if($newHeight < $heightAvatar){
+            $newHeight = $heightAvatar;
+            $newWidth = $newHeight * $ratio;
+        }
+
+        // Centraliza a imagem (horizontal e verticalmente)
+        $x = $widthAvatar - $newWidth;
+        $y = $heightAvatar - $newHeight;
+        $x = $x<0 ? $x/2 : $x;
+        $y = $y<0 ? $y/2 : $y;
+
+        $finalImage = imagecreatetruecolor($widthAvatar, $heightAvatar);
+        switch ($newAvatar['type']) {
+            case 'image/jpeg':
+            case 'image/jpg':
+                $image = imagecreatefromjpeg($newAvatar['tmp_name']);
+            break;
+            case 'image/png':
+                $image = imagecreatefrompng($newAvatar['tmp_name']);
+            break;
+        }
+
+        imagecopyresampled(
+            $finalImage, $image,
+            $x, $y, 0, 0,
+            $newWidth, $newHeight, $widthOrig, $heightOrig);
+
+        $avatarName = md5(time().rand(0,999)).'.jpg';
+
+        imagejpeg($finalImage, './media/avatars/'.$avatarName, 100);
+
+        $userInfo->avatar = $avatarName;
+    }
+}
 $user->update($userInfo);
     
 
